5f3bf98741c5203e49e1d3cf4778275234f14999
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-28 23:18:19 +0100

    Update current feature

--- wait-minio.sh
+++ wait-minio.sh
@@ -7,6 +7,7 @@
 set -euxo pipefail
 
 timeout=240
+wait_time=5
 
 while true
 do
@@ -15,10 +16,21 @@ do
   if [ "$endpoints" ]; then
     break
   fi
-  sleep 5
+  ink "Minio endpoints not found, retrying in $wait_time seconds"
+  sleep $wait_time
 done
 
-# Wait for minio to be ready
-# TODO: why wait so long?
-sleep 30
+ink "Wait for minio s3 bucket 'harbor' to exist"
+kubectl exec -it  s5cmd --env AWS_ACCESS_KEY_ID=minio --env  AWS_SECRET_ACCESS_KEY=minio123 --env S3_ENDPOINT_URL=https://minio.minio:443 --command -- sleep infinity
+
+while true
+do
+  kubectl exec -it  s5cmd -- /s5cmd --log debug --no-verify-ssl ls "s3://harbor/*"
+  if [ $? -eq 0 ]; then
+    break
+  else
+    ink "Bucket 'harbor' not found, retrying in $wait_time seconds"
+    sleep $wait_time
+  fi
+done
 
