06ca075e3968fb9ef4b3e370cb517c98fbceb823
Fabrice Jammes <fabrice.jammes@clermont.in2p3.fr>
2024-10-28 22:35:12 +0100

    Update current feature

--- wait-minio.sh
+++ wait-minio.sh
@@ -11,7 +11,7 @@ timeout=240
 while true
 do
   ink "Wait for minio s3 endpoint to be ready"
-  endpoints=$(kubectl get endpoints -l v1.min.io/tenant=minio -n minio -o jsonpath='{range .items[*]}{.subsets[*].addresses[*].ip}{'\n'}{end}')
+  endpoints=$(kubectl get endpoints -l v1.min.io/tenant=minio -n minio -o jsonpath="{range .items[*]}{.subsets[*].addresses[*].ip}{'\n'}{end}")
   if [ "$endpoints" ]; then
     break
   fi
